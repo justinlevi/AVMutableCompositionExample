22042b14252b9a5100a82908766a03a15bca0bd9
Justin Winter <justinlevi@gmail.com>
2015-09-18 12:54:07 -0400

    Initial Commit

---
+++ AVMutableCompositionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVMutableCompositionTest
+//
+//  Created by Justin Winter on 9/18/15.
+//  Copyright © 2015 wintercreative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
